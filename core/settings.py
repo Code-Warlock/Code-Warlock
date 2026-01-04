cf0f262578212965add938c11880130c7c307673
Code Warlock <dessymichaeltgp123@gmail.com>
2026-01-04 13:44:25 -0600

    Switch to safer WhiteNoise storage

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Cloudinary Configuration
