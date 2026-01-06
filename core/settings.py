22769d40c683c04a3c5474810fbdcbfaf1474dd4
Code Warlock <dessymichaeltgp123@gmail.com>
2026-01-06 04:09:47 -0600

    Fix installed apps order for Cloudinary

--- core/settings.py
+++ core/settings.py
@@ -35,14 +35,14 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-     'cloudinary_storage',
-    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'cloudinary_storage',
+    'cloudinary',
     'portfolio',
 ]
 
